9ff4c0d7cd6a3c10fdca906bbe15630484b18687
mcnc <ylro@mcnc.co.kr>
2023-03-13 14:31:01 +0900

    .

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -7,11 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//A
-		//B
-		//C
-		//D
-		//G
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
